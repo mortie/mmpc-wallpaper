0f69fe68f14cadf9ed313d8ee6e0a7e3d9750161
Martin Dørum <martid0311@gmail.com>
2018-08-26 00:58:31 +0200

    files with folders now two levels deep

--- server.js
+++ server.js
@@ -4,6 +4,37 @@ var imageSize = require("image-size");
 
 var conf = JSON.parse(fs.readFileSync("conf.json"));
 
+function thrImageSize(path, cb) {
+	function update() {
+		while (thrImageSize.count < thrImageSize.max && thrImageSize.queue.length > 0) {
+			var obj = thrImageSize.queue.pop();
+			thrImageSize.count += 1;
+			setTimeout(function() {
+				imageSize(obj.path, function(err, dimensions) {
+					thrImageSize.count -= 1;
+					update();
+					obj.cb(err, dimensions);
+				});
+			}, thrImageSize.timeout);
+		}
+	}
+
+	if (thrImageSize.count < thrImageSize.max) {
+		thrImageSize.count += 1;
+		imageSize(path, function(err, dimensions) {
+			thrImageSize.count -= 1;
+			update();
+			cb(err, dimensions);
+		});
+	} else {
+		thrImageSize.queue.push({ cb, path });
+	}
+}
+thrImageSize.count = 0;
+thrImageSize.defaultMax = thrImageSize.max = 5;
+thrImageSize.timeout = thrImageSize.defaultTimeout = 500;
+thrImageSize.queue = [];
+
 function parseTimeString(str) {
 	var total = 0;
 	var num = 0;
@@ -30,6 +61,11 @@ function random(min, max) {
 }
 
 function Async(n, func) {
+	if (n == 0) {
+		setTimeout(func, 0);
+		return null;
+	}
+
 	var arr = [];
 	return function(val) {
 		if (val)
@@ -44,7 +80,7 @@ function Async(n, func) {
 
 function Picture(path, cb) {
 	this.path = path;
-	imageSize(path, function(err, dimensions) {
+	thrImageSize(path, function(err, dimensions) {
 		if (err)
 			throw err;
 
@@ -54,6 +90,20 @@ function Picture(path, cb) {
 		if (cb)
 			cb();
 	}.bind(this));
+}
+Picture.prototype.serialize = function() {
+	return {
+		path: this.path,
+		dimensions: this.dimensions,
+		type: this.type,
+	};
+}
+Picture.unserialize = function(obj) {
+	let o = Object.create(Picture.prototype);
+	o.path = obj.path;
+	o.dimensions = obj.dimensions;
+	o.type = obj.type;
+	return o;
 }
 Picture.prototype.getType = function() {
 	var dims = this.dimensions;
@@ -98,6 +148,24 @@ function Pictures(dir, cb) {
 
 	this.update(cb);
 }
+Pictures.prototype.serialize = function() {
+	return {
+		dir: this.dir,
+		all: this.all.map(function(pic) { return pic.serialize(); }),
+		wide: this.wide.map(function(pic) { return pic.serialize(); }),
+		tall: this.tall.map(function(pic) { return pic.serialize(); }),
+		square: this.square.map(function(pic) { return pic.serialize(); }),
+	};
+}
+Pictures.unserialize = function(obj) {
+	var o = Object.create(Pictures.prototype);
+	o.dir = obj.dir;
+	o.all = obj.all.map(function(pic) { return Picture.unserialize(pic); });
+	o.wide = obj.wide.map(function(pic) { return Picture.unserialize(pic); });
+	o.tall = obj.tall.map(function(pic) { return Picture.unserialize(pic); });
+	o.square = obj.square.map(function(pic) { return Picture.unserialize(pic); });
+	return o;
+}
 Pictures.prototype.update = function(cb) {
 	console.log("Updating picture registry...");
 
@@ -108,8 +176,13 @@ Pictures.prototype.update = function(cb) {
 
 	var files = fs.readdirSync(this.dir);
 
+	var interval = setInterval(function() {
+		console.log("Still working on "+this.dir+": "+all.length+"/"+files.length);
+	}.bind(this), 10000);
+
 	var async = Async(files.length, function() {
-		console.log("Updated.");
+		clearInterval(interval);
+		console.log("Updated "+this.dir+".");
 
 		this.all = all;
 		this.wide = wide;
@@ -154,7 +227,7 @@ Pictures.prototype.getRandom = function(arr) {
 	var i = random(0, arr.length - 1);
 	return arr[i];
 };
-Pictures.prototype.setWallpaper = function() {
+Pictures.prototype.setWallpaper = function(cb) {
 	var picture = this.getRandom();
 
 	if (!picture)
@@ -178,16 +251,77 @@ Pictures.prototype.setWallpaper = function() {
 		var child = require("child_process").exec(conf.bg_changed_cmd);
 		child.stdout.pipe(process.stdout);
 		child.stderr.pipe(process.stderr);
+		cb();
 	});
 };
 
-var pictures = new Pictures(conf.path, function() {
-	pictures.setWallpaper();
-	setInterval(function() {
-		pictures.setWallpaper();
-	}, parseTimeString(conf.change_interval));
-});
+function serialize(pictureses, file) {
+	var obj = pictureses.map(function(p) { return p.serialize(); });
+	var json = JSON.stringify(obj);
+	fs.writeFileSync(file, json);
+	console.log("Serialized to "+file+".");
+}
+function unserialize(file) {
+	var json = fs.readFileSync(file);
+	var obj = JSON.parse(json);
+	let pictureses = obj.map(function(p) { return Pictures.unserialize(p); });
+	console.log("Unserialized "+file+".");
+	return pictureses;
+}
 
-setInterval(function() {
-	pictures.update();
-}, parseTimeString(conf.registry_update_interval));
+function startChange(pictureses, updateImmediately) {
+	let changeInterval = parseTimeString(conf.change_interval);
+	thrImageSize.max = 1;
+	thrImageSize.timeout = 1000;
+	function change() {
+		var pics;
+		do {
+			var r = Math.floor((Math.random() * pictureses.length));
+			var pics = pictureses[r];
+		} while (pics.all.length == 0);
+
+		console.log("Changing to random pic in "+pics.dir+"...");
+		pics.setWallpaper(function() {
+			thrImageSize.max = thrImageSize.defaultMax;
+			thrImageSize.timeout = thrImageSize.defaultTimeout;
+			console.log("Changed.");
+			setTimeout(change, changeInterval);
+		});
+	}
+	change();
+
+	let registryInterval = parseTimeString(conf.registry_update_interval);
+
+	function updateRegistry() {
+		console.log("Starting to update registry.");
+		var async = Async(pictureses.length, function() {
+			serialize(pictureses, "registry.json");
+			setTimeout(updateRegistry, registryInterval);
+		});
+		pictureses.forEach(function(p) { p.update(async); });
+	};
+
+	if (updateImmediately)
+		updateRegistry();
+	else
+		setTimeout(updateRegistry, registryInterval);
+}
+
+if (fs.existsSync("registry.json")) {
+	let pictureses = unserialize("registry.json");
+	startChange(pictureses, true);
+} else {
+	var subdirs = fs.readdirSync(conf.path).filter(function(x) { return x != "lost+found" });
+
+	var pictureses = [];
+
+	var async = Async(subdirs.length, function() {
+		serialize(pictureses, "registry.json");
+		startChange(pictureses, false);
+	});
+
+	subdirs.forEach(function(subdir) {
+		var pics = new Pictures(conf.path+"/"+subdir, async);
+		pictureses.push(pics);
+	});
+}
